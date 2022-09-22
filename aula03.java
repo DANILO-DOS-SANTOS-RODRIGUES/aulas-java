e9643311af4788c0b79df5146128085ebfb7aca1
Danilo <dev.danilo.rodrigues@gmail.com>
2022-09-22 20:02:32 -0300

    Declarando variáveis e concatenando para ser imprimido no terminal

--- aula03.java
+++ aula03.java
@@ -3,6 +3,7 @@ public class aula03 {
         String nome = "Danilo";
         Integer idade = 22;
         Double altura = 1.85;
+
         System.out.println("Nome: " + nome + "\n" + "Idade: " + idade + "\n" + "Altura: " + altura);
     }
 }
