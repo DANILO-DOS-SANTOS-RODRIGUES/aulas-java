feff6d0aa6273ab8add828ee5b76d61c5ce5697c
Danilo <dev.danilo.rodrigues@gmail.com>
2022-09-22 19:57:55 -0300

    Colocando o projeto do git e do github pelo terminal, testando fazer uma atualização no projeto

--- aula02.java
+++ aula02.java
@@ -1,5 +1,5 @@
 public class aula02 {
     public static void main(String[] args) {
-        System.out.println("Colocando projeto do git e do github, pelo terminal");
+        System.out.println("Colocando projeto do git e do github, pelo terminal.");
     }
 }
