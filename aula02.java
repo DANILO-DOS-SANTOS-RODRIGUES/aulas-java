5cf6515b1526d79014557f826a60210e7a8a8876
Danilo <dev.danilo.rodrigues@gmail.com>
2022-09-22 19:12:19 -0300

    Testando fazer o upload para o github dos codigos em outro computador para meu repositorio

--- aula02.java
+++
@@ -1,5 +0,0 @@
-public class aula02 {
-    public static void main(String[] args) {
-        System.out.println("Colocando projeto do git e do github, pelo terminal");
-    }
-}
--- aula03.java
+++
@@ -1,8 +0,0 @@
-public class aula03 {
-    public static void main(String[] args) {
-        String nome = "Danilo";
-        Integer idade = 22;
-        Double altura = 1.85;
-        System.out.println("Nome: " + nome + "\n" + "Idade: " + idade + "\n" + "Altura: " + altura);
-    }
-}
