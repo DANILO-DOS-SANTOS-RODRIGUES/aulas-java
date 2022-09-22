2f80e4d423c60479cae47f0ae4bfd5fdd0b0af92
DANILO-DOS-SANTOS-RODRIGUES <dev.danilo.rodrigues@gmail.com>
2022-09-21 22:40:16 -0300

    Colocando o projeto do git e do githup pelo terminal, testando fazer uma atualização no projeto

--- aula02.java
+++ aula02.java
@@ -1,5 +1,5 @@
 public class aula02 {
     public static void main(String[] args) {
-        System.out.println("testanto git e github");
+        System.out.println("Colocando projeto do git e do github, pelo terminal");
     }
 }
