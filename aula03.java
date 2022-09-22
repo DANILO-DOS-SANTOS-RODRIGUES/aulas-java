60021c1f16f73a135984c3158538f9704d04b4d3
Danilo <dev.danilo.rodrigues@gmail.com>
2022-09-22 19:16:22 -0300

    teste

---
+++ aula03.java
@@ -0,0 +1,8 @@
+public class aula03 {
+    public static void main(String[] args) {
+        String nome = "Danilo";
+        Integer idade = 22;
+        Double altura = 1.85;
+        System.out.println("Nome: " + nome + "\n" + "Idade: " + idade + "\n" + "Altura: " + altura);
+    }
+}
